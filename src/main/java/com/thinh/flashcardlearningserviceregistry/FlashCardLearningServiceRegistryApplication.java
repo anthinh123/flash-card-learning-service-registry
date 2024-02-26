4f9bb32fac3760a381d46b01c3caaeccf77ebe12
thinh <anvanthinh@gmail.com>
2024-02-26 15:22:55 +0700

    init

---
+++ src/main/java/com/thinh/flashcardlearningserviceregistry/FlashCardLearningServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.thinh.flashcardlearningserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardLearningServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardLearningServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinh/flashcardlearningserviceregistry/FlashCardLearningServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinh.flashcardlearningserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardLearningServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
